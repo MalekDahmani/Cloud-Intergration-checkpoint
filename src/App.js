c2d7944b1f6a03f82f51427dbd948c4cae6ef058
melek dahmani <YOUR EMAIL>
2022-10-15 21:54:00 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+        <h1 style={{color:'red'}}>hello</h1>
+        <p>This is GoMyCode Location</p>
+        <br />
+        <br />
+        <br />
+        <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=gomycode&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+       
+    </div>
+  );
+}
+
+export default App;
